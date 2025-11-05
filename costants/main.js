9610fed308d9ad62640040deaa36246a874b4894
irynasavina <irynasavina@gmail.com>
2025-11-05 20:10:10 +0200

    Fixed algorithms, icon

--- costants/main.js
+++ costants/main.js
@@ -3,10 +3,10 @@ export default {
         'OLL': [
             { name: 'Глаза', image: require('../assets/images/OLL/eyes.png'), algorithm: "R2 D' R U2 R' D R U2 R" },
             { name: 'Двойные глаза',  image: require('../assets/images/OLL/double_eyes.png'), algorithm: "R U R' U R U' R' U R U2 R'"},
-            { name: 'Вертолет',  image: require('../assets/images/OLL/helicopter.png'), algorithm: "R U2 R2' U' R2 U' R2' U2 R"},
+            { name: 'Вертолет',  image: require('../assets/images/OLL/helicopter.png'), algorithm: "R U2 R2 U' R2 U' R2 U2 R"},
             { name: 'Уши',  image: require('../assets/images/OLL/ears.png'), algorithm: "R U R D R' U' R D' R2"},
             { name: 'Восьмерка',  image: require('../assets/images/OLL/eight.png'), algorithm: "R' F R U R' U' R' F' R U R"},
-            { name: 'Рыбка вверх',  image: require('../assets/images/OLL/fishdw.png'), algorithm: "R U2 R' U' R U' R'"},
+            { name: 'Рыбка вверх',  image: require('../assets/images/OLL/fishdw.png'), algorithm: "R' U' R U' R' U2 R"},
             { name: 'Рыбка',  image: require('../assets/images/OLL/fish.png'), algorithm: "R U R' U R U2 R'"},
         ]
     }
